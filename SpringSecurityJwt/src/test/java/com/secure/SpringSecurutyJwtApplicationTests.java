4c0a42126fc7269b5c2f4960533b74937abc2057
vishalfirgan <vishalfirgan24@gmail.com>
2024-06-23 12:14:12 +0530

    First commit

---
+++ SpringSecurityJwt/src/test/java/com/secure/SpringSecurutyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurutyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
